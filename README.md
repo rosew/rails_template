b5198df2e6f98edda487b92f502b874ab8a53405
rosew <rose@parksidesoftware.com>
2019-11-23 21:50:03 -0700

    Configure devise with nice views

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # rails_template
-A template for creating a default rails app with some base configurations
+A template for creating a default rails app with some base configurations.
+
+Create a rails app with:
+
+```
+rails new <app_name> -m rails_template/template.rb --database=postgresql
+```
--- template.rb
+++ template.rb
@@ -32,19 +32,66 @@ directory "#{template_root}/app/assets", "app/assets"
 remove_file "app/views/layouts/application.html.erb"
 template "#{template_root}/app/views/layouts/application.html.erb.tt", "app/views/layouts/application.html.erb"
 
+inside 'app/helpers' do
+  inject_into_file 'application_helper.rb', after: "module ApplicationHelper\n" do
+    <<-RUBY
 
-create_file 'database.yml', "#{template_root}/config/database.yml"
+  def alert_colors(key)
+    case key
+    when "alert"
+      "ba b--red red"
+    when "notice"
+      "ba b--blue blue"
+    else
+      "bg-light-gray navy"
+    end
+  end
 
+    RUBY
+  end
+end
+
+# Setup static pages
 gem 'high_voltage' # Static pages
+empty_directory "app/views/pages"
+template "#{template_root}/app/views/pages/privacy.html.erb.tt", "app/views/pages/privacy.html.erb"
+template "#{template_root}/app/views/pages/terms.html.erb.tt", "app/views/pages/terms.html.erb"
+template "#{template_root}/app/views/pages/start.html.erb.tt", "app/views/pages/start.html.erb"
 
-gem "font-awesome-rails" # Icons
+# Set the homepage to the static start page
+inside 'config' do
+  inject_into_file 'routes.rb', after: "Rails.application.routes.draw do\n" do 
+    <<-RUBY
+  root to: "high_voltage/pages#show", id: 'start'
+    RUBY
+  end
+end
 
-gem 'devise' # Authentication
+# Authentication
+gem 'devise'
 generate "devise:install"
 generate "devise User"
+directory "#{template_root}/app/views/devise", "app/views/devise"
 environment "config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }", env: 'development'
 environment "config.action_mailer.default_url_options = { host: ENV['DOMAIN_NAME'], port: 3000 }", env: 'production'
 
+# Add confirmable and trackable to my devise model
+gsub_file "app/models/user.rb", "devise", "devise :confirmable, :trackable,"
+Dir.glob('db/migrate/*_create_users.rb').each do |file_name|
+  gsub_file file_name, "# t.integer  :sign_in_count", "t.integer  :sign_in_count"
+  gsub_file file_name, "# t.datetime :current_sign_in_at", "t.datetime :current_sign_in_at"
+  gsub_file file_name, "# t.datetime :last_sign_in_at", "t.datetime :last_sign_in_at"
+  gsub_file file_name, "# t.inet     :current_sign_in_ip", "t.inet     :current_sign_in_ip"
+  gsub_file file_name, "# t.inet     :last_sign_in_ip", "t.inet     :last_sign_in_ip"
+  gsub_file file_name, "# t.string   :confirmation_token", "t.string   :confirmation_token"
+  gsub_file file_name, "# t.datetime :confirmed_at", "t.datetime :confirmed_at"
+  gsub_file file_name, "# t.datetime :confirmation_sent_at", "t.datetime :confirmation_sent_at"
+  gsub_file file_name, "# t.string   :unconfirmed_email", "t.string   :unconfirmed_email"
+  gsub_file file_name, "# add_index :users, :confirmation_token", "add_index :users, :confirmation_token"
+end
+
 gem 'pundit' # Authorization
+
+# Other useful gems
 gem 'sendgrid-ruby' # Sendgrid for sending email
 
